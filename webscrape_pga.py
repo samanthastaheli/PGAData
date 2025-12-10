7f220872c55b984ee97edb9fdd32cf9ff18631da
samanthastaheli <sstaheli27@gmail.com>
2025-12-09 20:17:32 -0700

    added

--- webscrape_pga.py
+++ webscrape_pga.py
@@ -2,17 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-url = "https://www.pgatour.com/stats"
+# https://www.geeksforgeeks.org/python/python-web-scraping-tutorial/
 
-res = requests.get(url)
-print(res.status_code)
-# print(res.content)
+def request_stats():
+    url = "https://www.pgatour.com/stats"
 
-soup = BeautifulSoup(res.content, "html.parser")
-with open("stats.xml", "w") as file:
-    file.write(soup.prettify())
+    res = requests.get(url)
+    print(res.status_code)
+    # print(res.content)
+    save_xml_to_file(res.content, "stats.xml")
+
+def save_xml_to_file(content, filepath):
+    soup = BeautifulSoup(content, "html.parser")
+    with open(filepath, "w") as file:
+        file.write(soup.prettify())
 
 
 # Get info from stats page
 # <a role="link" class="chakra-linkbox__overlay css-1hnz6hu" href="/player/46046/scottie-scheffler/stats">Scottie Scheffler</a>
 # <span class="chakra-text css-nxrnrc">68.131</span>
+
+
+if __name__ == "__main__":
+    request_stats()
