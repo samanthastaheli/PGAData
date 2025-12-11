bee1ae7386c1eb2b5c192084d9a1a4568a7b58e8
Samantha Staheli <sstaheli27@gmail.com>
2025-12-10 21:00:49 -0700

    got players stats data

--- webscrape_pga.py
+++ webscrape_pga.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import os
 from lxml import etree, html
 import json
+import pandas
 
 # https://www.geeksforgeeks.org/python/python-web-scraping-tutorial/
 # https://www.geeksforgeeks.org/python/how-to-use-lxml-with-beautifulsoup-in-python/
@@ -43,6 +44,8 @@ def request_player_stats():
     url = "https://www.pgatour.com/player/46046/scottie-scheffler/stats"
     # "/player/"
 
+# region player ids json
+
 def get_player_ids():
     """
     Player info is in stats html as:
@@ -71,6 +74,12 @@ def get_player_ids():
     with open("sources/players.json", "w") as file:
         json.dump(players, file, indent=4)
 
+# endregion
+
+# region player stats
+
+
+
 def get_player_stats():
     """
     Every players stats page has:
@@ -84,34 +93,99 @@ def get_player_stats():
     "statId":"337","rank":"11","value":"30' 2","title":"Approaches from 175-200 yards","category":["APPROACH"],"aboveOrBelow":"EVEN","fieldAverage":"","supportingStat":{
     """
     players_dict = load_json("sources/players.json")
-    player_one = next(iter(players_dict))
-    full_url = BASE_URL + players_dict[player_one]["url"]
-    print(f"full url: {full_url}")
-    content = make_request(full_url)
 
-    tree = html.fromstring(content)
-    script_tags = tree.xpath("id('__NEXT_DATA__')")[0]
-    data_dict = json.loads(script_tags.text)
-    # Save to json file
-    # with open("sources/data_dict.json", "w") as file:
-    #     json.dump(data_dict, file, indent=4)
+    # initilaize pandas dataframe
+    data_columns = [
+        "playerId", "player", "statId", "title", "value", 
+        "rank", "category", "aboveOrBelow", "fieldAverage", 
+        "supportingStatDescription", "supportingStatValue", 
+        "supportingValueDescription", "supportingValueValue"
+    ]
+    dataframe = pandas.DataFrame(columns=data_columns)
+    new_rows = []
 
-    print(data_dict.keys())
-    queries = data_dict['props']['pageProps']['dehydratedState']['queries']
-    for i, q in enumerate(queries):
-        looking_for = 'playerProfileStatsFull'
-        print(i)
-        q_data = q['state']['data']
-        if isinstance(q_data, dict):
-            if looking_for in q_data.keys():
-                print(q_data[looking_for])
-                with open("sources/playerProfileStatsFull_dict.json", "w") as file:
-                    json.dump(q_data[looking_for], file, indent=4)
+    # Get every players info
+    for player_name, value in players_dict.items():
+        player_id = value["id"]
 
-        # if "statid" in q:
-        #     print(q)
+        # Get url and make request
+        full_url = BASE_URL + value["url"]
+        print(f"full url: {full_url}")
+        content = make_request(full_url)
+
+        # Parse request content
+        # all stat data is located in a script tag with id '__NEXT_DATA__'
+        tree = html.fromstring(content)
+        script_tags = tree.xpath("id('__NEXT_DATA__')")[0]
+        data_dict = json.loads(script_tags.text)
+
+        # work way through dict to get stat info
+        queries = data_dict['props']['pageProps']['dehydratedState']['queries']
+        for q in queries:
+            looking_for = 'playerProfileStatsFull'
+            q_data = q['state']['data']
+            if isinstance(q_data, dict):
+                if looking_for in q_data.keys():
+                    player_stats_dict = q_data[looking_for][0]
+                    stats_list = player_stats_dict['stats']
+                    for stats in stats_list:
+                        new_row = get_player_new_row(player_name, player_id, data_columns, stats)
+                        new_rows.append(new_row)
+
+    # Add new rows to dataframe
+    for row in new_rows:
+        dataframe.loc[len(dataframe)] = row
+
+    # save the dataframe
+    dataframe.to_csv('data/player_stats.csv', header=True, index=False)
 
 
+def get_player_new_row(player_name, player_id, data_columns, stats):
+    # initialize new row
+    new_row = data_columns.copy()
+
+    # Add player info
+    new_row[new_row.index("playerId")] = player_id
+    new_row[new_row.index("player")] = player_name
+
+    for key, value in stats.items():
+        if key == "statId":
+            new_row[new_row.index("statId")] = value
+        elif key == "rank":
+            new_row[new_row.index("rank")] = value
+        elif key == "value":
+            new_row[new_row.index("value")] = value
+        elif key == "title":
+            new_row[new_row.index("title")] = value
+        elif key == "category":
+            new_row[new_row.index("category")] = value
+        elif key == "aboveOrBelow":
+            new_row[new_row.index("aboveOrBelow")] = value
+        elif key == "fieldAverage":
+            new_row[new_row.index("fieldAverage")] = value
+        elif key == "supportingStat":
+            if value:
+                new_row[new_row.index("supportingStatDescription")] = value["description"]
+                new_row[new_row.index("supportingStatValue")] = value["value"]
+            else:
+                new_row[new_row.index("supportingStatDescription")] = value
+                new_row[new_row.index("supportingStatValue")] = value
+        elif key == "supportingValue":
+            if value:
+                new_row[new_row.index("supportingValueDescription")] = value["description"]
+                new_row[new_row.index("supportingValueValue")] = value["value"]
+            else:
+                new_row[new_row.index("supportingValueDescription")] = value
+                new_row[new_row.index("supportingValueValue")] = value
+        else:
+            print(f"\033[33mWARNING\033[0m Unknown key: {key}")
+
+    # Check every column got filled
+    for i in range(len(data_columns)):
+        new_row[i] != data_columns[i]
+    return new_row
+
+# endregion
 
 if __name__ == "__main__":
     # request_stats()
